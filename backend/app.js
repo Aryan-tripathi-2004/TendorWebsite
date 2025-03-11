f9b81d160e48e3ae9d00a70224002c081d44b4d4
Om Datt Tripathi <aryantripathi1606@gmail.com>
2025-03-11 16:52:28 +0530

    [Om Datt Tripathi] Add. add folders and dependencies

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,10 @@
 const express = require("express");  
 const app = express();  
 
-app.get("/", (req, res) => {  
-  res.send("Hello, Node.js!");  
-});  
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 app.listen(3000, () => {  
   console.log("Server running on http://localhost:3000");  
-});  
+});
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,11 @@
+import { Button } from "@/components/ui/button"
+
 function App() {
   return (
-    <h1>welcome to tendor App</h1>
+    <>
+      <h1 className="text-5xl font-bold text-red-600 mx-auto underline">welcome to tendor App</h1>
+      <Button className="bg-blue-500 text-white" >Click me</Button>
+    </>
   )
 }
 
